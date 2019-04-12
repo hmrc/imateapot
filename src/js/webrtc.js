2bbc0a64398550ebadbe3ca9faf6ae96a8734dd8
Benjamin Parker <benjamin@needstyping.com>
2019-04-12 14:00:11 +0100

    Hangup close remote

--- src/js/webrtc.js
+++ src/js/webrtc.js
@@ -6,112 +6,119 @@ var uuid;
 var serverConnection;
 
 var peerConnectionConfig = {
-  'iceServers': [
-    {'urls': 'stun:stun.stunprotocol.org:3478'},
-    {'urls': 'stun:stun.l.google.com:19302'},
-  ]
+    'iceServers': [
+        {'urls': 'stun:stun.stunprotocol.org:3478'},
+        {'urls': 'stun:stun.l.google.com:19302'},
+    ]
 };
 
 function pageReady() {
-  console.log("pageReady")
-  uuid = createUUID();
+    console.log("pageReady")
+    uuid = createUUID();
 
-  localVideo = document.getElementById('localVideo');
-  remoteVideo = document.getElementById('remoteVideo');
+    localVideo = document.getElementById('localVideo');
+    remoteVideo = document.getElementById('remoteVideo');
 
-  serverConnection = new WebSocket('ws://192.168.160.148:8010');
-  serverConnection.onmessage = gotMessageFromServer;
+    serverConnection = new WebSocket('ws://192.168.160.148:8010');
+    serverConnection.onmessage = gotMessageFromServer;
 
-  var constraints = {
-    video: true,
-    audio: true,
-  };
+    var constraints = {
+        video: true,
+        audio: true,
+    };
 
-  if(navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia(constraints).then(getUserMediaSuccess).catch(errorHandler);
-  } else {
-    alert('Your browser does not support getUserMedia API');
-  }
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(constraints).then(getUserMediaSuccess).catch(errorHandler);
+    } else {
+        alert('Your browser does not support getUserMedia API');
+    }
 }
 
 function getUserMediaSuccess(stream) {
-  localStream = stream;
-  localVideo.srcObject = stream;
+    localStream = stream;
+    localVideo.srcObject = stream;
 }
 
 function call(isCaller) {
-  console.log("Starting call isCaller: " + isCaller)
-  peerConnection = new RTCPeerConnection(peerConnectionConfig);
-  peerConnection.onicecandidate = gotIceCandidate;
-  peerConnection.ontrack = gotRemoteStream;
-  peerConnection.addStream(localStream);
+    console.log("Starting call isCaller: " + isCaller)
+    remoteVideo.style.visibility = "visible";
+    peerConnection = new RTCPeerConnection(peerConnectionConfig);
+    peerConnection.onicecandidate = gotIceCandidate;
+    peerConnection.ontrack = gotRemoteStream;
+    peerConnection.addStream(localStream);
 
-  if(isCaller) {
-    peerConnection.createOffer().then(createdDescription).catch(errorHandler);
-  }
+    if (isCaller) {
+        peerConnection.createOffer().then(createdDescription).catch(errorHandler);
+    }
 }
 
 function hangup() {
-  console.log("Hangup call");
-  serverConnection.send(JSON.stringify({'hangup': 'true'}));
-  peerConnection.close();
+    console.log("Hangup call");
+    serverConnection.send(JSON.stringify({'hangup': 'true'}));
+    closeRemote();
+}
+
+function closeRemote() {
+    peerConnection.close();
+    peerConnection = null;
+    remoteVideo.style.visibility = "hidden";
 }
 
 function gotMessageFromServer(message) {
-  console.log("WebSocket>> " + JSON.stringify(message))
-  if(!peerConnection) call(false);
+    console.log("WebSocket>> " + JSON.stringify(message))
+    if (!peerConnection) call(false);
 
-  var signal = JSON.parse(message.data);
+    var signal = JSON.parse(message.data);
 
-  // Ignore messages from ourself
-  if(signal.uuid == uuid) return;
+    // Ignore messages from ourself
+    if (signal.uuid == uuid) return;
 
-  if(signal.sdp) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
-      // Only create answers in response to offers
-      //console.log("WebSocket>> ICE Candidate" + JSON.stringify(signal))
-      if(signal.sdp.type == 'offer') {
-        peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
-      }
-    }).catch(errorHandler);
-  } else if(signal.ice) {
-    //console.log("WebSocket>> ICE Candidate: " + JSON.stringify(signal))
-    peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
-  } else if (signal.hangup) {
-    peerConnection.close();
-  }
+    if (signal.sdp) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function () {
+            // Only create answers in response to offers
+            //console.log("WebSocket>> ICE Candidate" + JSON.stringify(signal))
+            if (signal.sdp.type == 'offer') {
+                peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
+            }
+        }).catch(errorHandler);
+    } else if (signal.ice) {
+        //console.log("WebSocket>> ICE Candidate: " + JSON.stringify(signal))
+        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+    } else if (signal.hangup) {
+        closeRemote();
+    }
 }
 
 function gotIceCandidate(event) {
-  if(event.candidate != null) {
-    console.log("gotIceCandidate from ICE server: " + JSON.stringify(event.candidate));
-    serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid}));
-  }
+    if (event.candidate != null) {
+        console.log("gotIceCandidate from ICE server: " + JSON.stringify(event.candidate));
+        serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid}));
+    }
 }
 
 function createdDescription(description) {
-  console.log("createdDescription" + + JSON.stringify(event));
+    console.log("createdDescription" + +JSON.stringify(event));
 
-  peerConnection.setLocalDescription(description).then(function() {
-  serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
-  }).catch(errorHandler);
+    peerConnection.setLocalDescription(description).then(function () {
+        serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
+    }).catch(errorHandler);
 }
 
 function gotRemoteStream(event) {
-  console.log("gotRemoteStream: " + JSON.stringify(event));
-  remoteVideo.srcObject = event.streams[0];
+    console.log("gotRemoteStream: " + JSON.stringify(event));
+    remoteVideo.srcObject = event.streams[0];
 }
 
 function errorHandler(error) {
-  console.log(error);
+    console.log(error);
 }
 
 // Taken from http://stackoverflow.com/a/105074/515584
 // Strictly speaking, it's not a real UUID, but it gets the job done here
 function createUUID() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  }
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
 
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
